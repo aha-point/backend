03297bd2af7e4ff66d67091f4c5fd6197f2146b4
jiaLEE <jia9510@naver.com>
2023-03-06 23:49:47 +0900

    404 에러 해결.

--- src/main/java/com/member/MemberApplication.java
+++ src/main/java/com/member/MemberApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"java"})
+@ComponentScan(basePackages = {"com.member.controller"})
 public class MemberApplication {
 
 	public static void main(String[] args) {
