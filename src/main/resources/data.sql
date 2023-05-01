77c57d569727e7287dea22698908d79ee1080c4b
jiaLEE <jia9510@naver.com>
2023-05-01 18:39:47 +0900

    jpa data.sql와 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+create table category_code (
+ code varchar(5) not null,
+ category varchar(12) not null
+);
+
+insert into category_code values ('C0001',	'한식');
+insert into category_code values ('C0002',	'치킨');
+insert into category_code values ('C0003',	'분식');
+insert into category_code values ('C0004',	'돈까스');
+insert into category_code values ('C0005',	'족발/보쌈');
+insert into category_code values ('C0006',	'찜/탕');
+insert into category_code values ('C0007',	'구이');
+insert into category_code values ('C0008',	'피자');
+insert into category_code values ('C0009',	'중식');
+insert into category_code values ('C0010',	'양식');
+insert into category_code values ('C0011',	'커피/차');
+insert into category_code values ('C0012',	'디저트');
+insert into category_code values ('C0013',	'간식');
+insert into category_code values ('C0014',	'아시안');
+insert into category_code values ('C0015',	'샌드위치');
+insert into category_code values ('C0016',	'샐러드');
+insert into category_code values ('C0017',	'버거');
+insert into category_code values ('C0018',	'멕시칸');
+insert into category_code values ('C0019',	'도시락');
+insert into category_code values ('C0020',	'죽');
+insert into category_code values ('C0021',	'프랜차이즈');
